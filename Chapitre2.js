157ae004f58c42f43ca860559e552f301a23f6fb
BlackFox3000 <flo-ti@hotmail.fr>
2019-10-14 14:19:35 +0200

    Exercice 3, terminé

--- Chapitre2.js
+++ Chapitre2.js
@@ -33,9 +33,10 @@ function printPair(n){
         //alert(n+' n\'est pas pair');
 }
 
-function exercice3(a,b,c) {
-    var tab= [a,b,c];
+function exercice3(tab) {
+    var a=tab[0],b=tab[1],c=tab[2];
     var desctab=desc(tab);
+    document.write(printTab(tab)+'<br>');
     document.write('A: '+a+' B: '+b+' C: '+c+' <br>' +
         'Somme: '+ somme(tab)+'<br>' +
         ' Moyenne: '+moyenne(tab)+'<br> '+
@@ -91,33 +92,55 @@ function desc(tab) {
         return tab;
     var descTab=[];
     //parcourt de la table
-   // document.write('descde '+printTab(tab)+' <br>');
-    tab.forEach(function (element) {
+  //  document.write('tab: '+printTab(tab)+' <br>tab.length= '+(descTab.length*1+1)+'<br>');
+    for(var j=0; j<tab.length; j++) {
+        element=tab[j];
        // document.write('element: '+element+'<br>');
+       // document.write('desctabLength='+descTab.length+'<br>');
+        let length=descTab.length*1+1;
+        for(let i=0; i<length; i++) {
+            if (descTab.length*1===0) {
+                descTab.push(element);
 
-        for(var i=0; i<descTab.length+1; i++) {
-            document.write('desctab de '+i+' ; '+descTab[i]+'<br>');
+               //  document.write('je suis indéfini: => <br>');
+              // document.write('desctab de '+i+'= '+descTab[i]+'<br>');
 
-            if (descTab[i] == undefined) {
-                var plop=element*1;
-                //descTab[i]=8 ;
-                document.write('je suis indéfini:'+ element*1);
-                document.write('desctab de '+i+' ; '+descTab[i]+'<br>');
-
-            }/* else if (descTab[i] > element) {
-                var save=element;
+            } else if (descTab[i] > element) {
+                var save;
                 do {
+               //     document.write('desctab de '+i+'= '+descTab[i]+'<br>');
                      save = descTab[i];
-                    document.write('save:'+save+'<br>');
-                    descTab[i] = save;
+                 //   document.write('save:'+save+'<br>');
+                    descTab[i] = element;
                     i++;
+                    if(save!=undefined)
+                        element=save;
                 }
-                while (save!=undefined && i<5)
-            }*/
+                while (save!=undefined)
+            }else if(descTab[i]<element && descTab[i+1]==undefined){
+                i++;
+                descTab[i]=element;
+              //  document.write('desctab de '+i+'= '+descTab[i]+'<br>');
+            }
 
-          //  document.write('desctab de '+i+' ; '+descTab[i]+'<br>');
+           // document.write('_________________________________________<br>');
         }
-    })
+    }
+
     return descTab;
 }
 
+function initialisationTab() {
+    var i=0, tab=[];
+    var entrer=prompt('Entrer un nombre pour ajouter une variable à votre tableau sinon écrire "stop"./n' +
+        'Initialisation de la case n°'+i);
+    if (entrer !== 'stop') {
+        do {
+            tab.push(entrer*1);
+            i++;
+            entrer = prompt('Entrer un nombre pour ajouter une variable à votre tableau sinon écrire "stop"./n' +
+                'Initialisation de la case n°' + i);
+        } while (entrer !== 'stop')
+    }
+    return tab;
+}
