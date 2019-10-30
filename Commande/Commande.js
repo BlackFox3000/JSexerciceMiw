fa0e2c2e37e1ba80788ed86d9af18a2bb9677dc0
BlackFox3000 <flo-ti@hotmail.fr>
2019-10-30 09:19:58 +0100

    Commande ajout du plus et moins avec calcule du cas >0

--- Commande/Commande.js
+++ Commande/Commande.js
@@ -1,22 +1,34 @@
-var libProd=['lait','cereales', 'pancake'], prxProd;
+var libProd=['lait','cereales', 'pancake'], prxProd=[1.70,2.90,4.90];
 
 /**
  * afficher le formulaire.
  */
 function afficheForm() {
-    let ch='<form>'
+    let ch='<form name="indentification">'
+        +'<label for="nom">Nom: </label>'
+        +'<input type="text" name="nom" id="nom" required>'
+        +'<label for="prenom">Prénom: </label>'
+        +'<input type="text" name="prenom" id="prenom" required>'
+        +'<br>'
+        +'<label for="adresse">Adresse: </label>'
+        +'<input type="text" name="adresse" id="adresse" required>'
+        +'<br>'
         +'<label for="codePostal">Code Postal: </label>'
         +'<input type="text" name="codePostal" id="codePostal" required>'
+        +'<label for="ville">Ville: </label>'
+        +'<input type="text" name="ville" id="ville" required>'
         +'<br>'
         +'<label for="tel">Numéro de télephone: </label>'
         +'<input type="text" name="tel" id="tel" required>'
-        +'<br>'
         +'<label for="mail">mail: </label>'
         +'<input type="text" name="mail" id="mail" required>'
-        +'<br>'
+        +'<br> </form>'
     ;
 
-    let menuDeroulant=generatorListeDeroulante();
+    let menuDeroulant=  '<br><form name="produit" >\n' +generatorListeDeroulante()+'<input name="prix" value=0 readonly>' +
+        ' <input type="button" value="+" onclick="plus(document.produit.produits.value)">' +
+        ' <input type="button" value="-" onclick="moins(document.produit.produits.value)"> ' +
+        '</form>';
 
 
     document.getElementsByTagName("body")[0].innerHTML=ch+menuDeroulant;
@@ -28,8 +40,10 @@ function afficheForm() {
  * @param n
  * @returns {boolean}
  */
-function plus(n){
-    return false;
+function plus(value) {
+    if (Number(document.produit.prix.value).toFixed(2) != 0 && Number(document.produit.prix.value != undefined).toFixed(2)) {
+        document.produit.prix.value = Number(prxProd[value] * 1 + document.produit.prix.value * 1).toFixed(2);
+    }
 }
 
 /**
@@ -38,9 +52,17 @@ function plus(n){
  * @param n
  * @returns {boolean}
  */
-function moins(n){
-    return false;
+function moins(value) {
+    if (document.produit.prix.value*1 > value*1 && document.produit.prix.value!=0) {
+        document.produit.prix.value= Number(document.produit.prix.value-prxProd[value]).toFixed(2);
+        if( document.produit.prix.value==0 || document.produit.prix.value<0) {
+            document.produit.produits.value = -1;
+            document.produit.prix.value=0;
+        }
+
+    }
 }
+
 
 /**
  * La fonction sup(n) effacera les données de la ligne n. Elle sera appelée quand on cliquera sur
@@ -58,6 +80,7 @@ function sup(n) {
  * @returns {boolean}
  */
 function affMontant() {
+    //document.produit.prix.value;
     return false;
 }
 
@@ -112,12 +135,20 @@ function mailIsValide(mail) {
 
 function generatorListeDeroulante() {
     let menu=
-    '<br><form>\n' +
-    '    <select id="produits" name="produits" onchange="map(this.value)">\n' +
-    '        <option>----</option>\n';
+    '    <select id="produits" name="produits" onchange="takePrice(this.value)">\n' +
+    '        <option value=-1>----</option>\n';
     for(let i=0; i<libProd.length; i++){
-        menu += '<option value="'+libProd[i]+'">'+libProd[i]+'</option>';
+        menu += '<option value="'+i+'">'+libProd[i]+'</option>';
     }
-    menu += '    </select> </form>'
+
     return menu;
 }
+
+/**
+ * Place le prix correspondant au produit sélectionner dans la case prix
+ * @param value
+ */
+function takePrice(value) {
+   document.produit.prix.value=prxProd[value];
+}
+
