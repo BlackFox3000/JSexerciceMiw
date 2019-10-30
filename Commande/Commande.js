4c14fd21fadfa17a10d72e8f009f0384475489c0
BlackFox3000 <flo-ti@hotmail.fr>
2019-10-30 18:24:13 +0100

    Commande récupérer le prix en fonction de la ligne

--- Commande/Commande.js
+++ Commande/Commande.js
@@ -1,43 +1,40 @@
-var libProd=['lait','cereales', 'pancake'], prxProd=[1.70,2.90,4.90], nbProduit=0;
+var libProd = ['lait', 'cereales', 'pancake'], prxProd = [1.70, 2.90, 4.90], nbProduit = 0;
 
 /**
  * afficher le formulaire.
  */
 function afficheForm() {
-    let ch='<form name="indentification">'
-        +'<label for="nom">Nom: </label>'
-        +'<input type="text" name="nom" id="nom" required>'
-        +'<label for="prenom">Prénom: </label>'
-        +'<input type="text" name="prenom" id="prenom" required>'
-        +'<br>'
-        +'<label for="adresse">Adresse: </label>'
-        +'<input type="text" name="adresse" id="adresse" required>'
-        +'<br>'
-        +'<label for="codePostal">Code Postal: </label>'
-        +'<input type="text" name="codePostal" id="codePostal" required>'
-        +'<label for="ville">Ville: </label>'
-        +'<input type="text" name="ville" id="ville" required>'
-        +'<br>'
-        +'<label for="tel">Numéro de télephone: </label>'
-        +'<input type="text" name="tel" id="tel" required>'
-        +'<label for="mail">mail: </label>'
-        +'<input type="text" name="mail" id="mail" required>'
-        +'<br> </form>'
+    let authentification = '<form name="indentification">'
+        + '<label for="nom">Nom: </label>'
+        + '<input type="text" name="nom" id="nom" required>'
+        + '<label for="prenom">Prénom: </label>'
+        + '<input type="text" name="prenom" id="prenom" required>'
+        + '<br>'
+        + '<label for="adresse">Adresse: </label>'
+        + '<input type="text" name="adresse" id="adresse" required>'
+        + '<br>'
+        + '<label for="codePostal">Code Postal: </label>'
+        + '<input type="text" name="codePostal" id="codePostal" required>'
+        + '<label for="ville">Ville: </label>'
+        + '<input type="text" name="ville" id="ville" required>'
+        + '<br>'
+        + '<label for="tel">Numéro de télephone: </label>'
+        + '<input type="text" name="tel" id="tel" required>'
+        + '<label for="mail">mail: </label>'
+        + '<input type="text" name="mail" id="mail" required>'
+        + '<br> </form>'
     ;
-    let addProduit='<input type="button" value="Ajouter produit" onclick="affLigne()">'
 
+    /*  let menuDeroulant=  '<br><form name="produit" >\n' +generatorListeDeroulante()+'<input name="prix" value=0 readonly>' +
+          ' <input type="button" value="+" onclick="plus(document.produit.produits.value)">' +
+          ' <input type="button" value="-" onclick="moins(document.produit.produits.value)"> ' +
+          ' <input type="button" value="x" onclick="sup(document.produit.produits.value)"> ' +
+          '</form>';
 
+  */
+    document.getElementsByTagName("body")[0].innerHTML = authentification + '<div class="liste"></div>';
+    affLigne();
 
-
-
-  /*  let menuDeroulant=  '<br><form name="produit" >\n' +generatorListeDeroulante()+'<input name="prix" value=0 readonly>' +
-        ' <input type="button" value="+" onclick="plus(document.produit.produits.value)">' +
-        ' <input type="button" value="-" onclick="moins(document.produit.produits.value)"> ' +
-        ' <input type="button" value="x" onclick="sup(document.produit.produits.value)"> ' +
-        '</form>';
-
-*/
-    document.getElementsByTagName("body")[0].innerHTML=ch+addProduit+'<div class="liste">test</div>'//+menuDeroulant;
 }
 
 /**
@@ -47,10 +44,12 @@ function afficheForm() {
  * @returns {boolean}
  */
 function plus(n) {
-    let value= document.n.produits.value
-    if (Number(document.n.prix.value).toFixed(2) != 0 && Number(document.n.prix.value != undefined).toFixed(2)) {
-        document.n.prix.value = Number(prxProd[value] * 1 + document.n.prix.value * 1).toFixed(2);
+    n = 'carte' + n;
+    let value = document.forms[n].produits.value;
+    if (Number(document.forms[n].prix.value).toFixed(2) != 0 && Number(document.forms[n].prix.value != undefined).toFixed(2)) {
+        document.forms[n].prix.value = Number(prxProd[value] * 1 + document.forms[n].prix.value * 1).toFixed(2);
     }
+    document.forms[n].quantite.value++;
 }
 
 /**
@@ -59,12 +58,18 @@ function plus(n) {
  * @param n
  * @returns {boolean}
  */
-function moins(value) {
-    if (document.produit.prix.value*1 > value*1 && document.produit.prix.value!=0) {
-        document.produit.prix.value= Number(document.produit.prix.value-prxProd[value]).toFixed(2);
-        if( document.produit.prix.value==0 || document.produit.prix.value<0) {
-            sup(value)
+function moins(n) {
+    n = 'carte' + n;
+    //console.log(document.forms[n]);
+    let value = document.forms[n].produits.value;
+    if (document.forms[n].prix.value * 1 > value * 1 && document.forms[n].prix.value != 0) {
+        document.forms[n].prix.value = Number(document.forms[n].prix.value - prxProd[value]).toFixed(2);
+        document.forms[n].quantite.value--;
+        if (document.forms[n].prix.value == 0 || document.forms[n].prix.value < 0) {
+            console.log(n.split('carte')[1])
+            sup(parseInt(n.split('carte')[1]))
         }
+
 
     }
 }
@@ -77,8 +82,11 @@ function moins(value) {
  * @returns {boolean}
  */
 function sup(n) {
-    document.produit.produits.value = -1;
-    document.produit.prix.value=0;
+    n = 'carte' + n
+    console.log(n)
+    document.forms[n].produits.value = -1;
+    document.forms[n].prix.value = 0;
+    document.forms[n].quantite.value =0;
 }
 
 /**
@@ -107,15 +115,19 @@ function validationCmd() {
  * @param n
  */
 function affLigne() {
-
-    let form='<br><form name="carte'+nbProduit+'" >\n' +generatorListeDeroulante("carte"+nbProduit)+'<input name="prix" value=0 readonly>' +
-    ' <input type="button" value="+" onclick="plus("carte"+nbProduit)">' +
-    ' <input type="button" value="-" onclick="moins(document.'+nbProduit+'.produits.value)"> ' +
-    ' <input type="button" value="x" onclick="sup(document.'+nbProduit+'.produits.value)"> ' +
-    '</form>';
+    let index = nbProduit, nom='carte';
+    let form = '<br>' + nbProduit + '<form name="carte' + nbProduit + '" id="carte' + index + '"  >\n' +
+        generatorListeDeroulante("carte" + index) +
+        '<input name="prix" value=0 readonly>' +
+        '<input name="quantite" value=0 readonly>'+
+        ' <input type="button" value="+" onclick="plus(' + index + ')">' +
+        ' <input type="button" value="-" onclick="moins(' + index + ')"> ' +
+        ' <input type="button" value="x" onclick="sup(' +index + ')"> ' +
+        '</form>';
     nbProduit++;
-    document.getElementsByClassName("liste")[0].innerHTML+=form;
-  //  document.getElementsByClassName("liste")[]
+    let wrapper = document.createElement("div");
+    wrapper.innerHTML = form;
+    document.getElementsByClassName("liste")[0].appendChild(wrapper);
 }
 
 /**
@@ -124,7 +136,7 @@ function affLigne() {
  * @returns {boolean}
  */
 function codePostalIsValide(code) {
-    let reg= new RegExp(/^[0-9]{5}/)
+    let reg = new RegExp(/^[0-9]{5}/)
     return reg.test(code);
 }
 
@@ -133,8 +145,8 @@ function codePostalIsValide(code) {
  * caractère espace ou le tiret (on ne pourra pas mélanger des espaces et des tirets)
  */
 function numTelIsValide(numTel) {
-    let reg=new RegExp(/^[0-9]{2}(\s[0-9]{2}){4}$/);
-    let reg2=new RegExp(/^[0-9]{2}(-[0-9]{2}){4}$/);
+    let reg = new RegExp(/^[0-9]{2}(\s[0-9]{2}){4}$/);
+    let reg2 = new RegExp(/^[0-9]{2}(-[0-9]{2}){4}$/);
     return (reg.test(numTel) || reg2.test(numTel));
 }
 
@@ -144,7 +156,7 @@ function numTelIsValide(numTel) {
  * @returns {boolean}
  */
 function mailIsValide(mail) {
-    let reg=new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z]+\.[a-zA-Z0-9]{2,4}$/);
+    let reg = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z]+\.[a-zA-Z0-9]{2,4}$/);
     return reg.test(mail);
 }
 
@@ -152,13 +164,12 @@ function mailIsValide(mail) {
  * Génère la liste déroulante pour un bouton
  * @returns {string}
  */
-function generatorListeDeroulante(n){
-    console.log(n);
-    let menu=
-    '    <select id="produits" name="produits" onchange="document.'+n+'.prix.value=prxProd[this.value]">\n' +
-    '        <option value=-1>--Choisir--</option>\n';
-    for(let i=0; i<libProd.length; i++){
-        menu += '<option value="'+i+'">'+libProd[i]+'</option>';
+function generatorListeDeroulante(n) {
+    let menu =
+        '    <select id="produits" name="produits" onchange="takePrice(this.value, ' + n + ')">\n' +
+        '        <option value=-1>--Choisir--</option>\n';
+    for (let i = 0; i < libProd.length; i++) {
+        menu += '<option value="' + i + '">' + libProd[i] + '</option>';
     }
 
     return menu;
@@ -166,10 +177,18 @@ function generatorListeDeroulante(n){
 
 /**
  * Place le prix correspondant au produit sélectionner dans la case prix
- * Inutilisé
  * @param value
  */
 function takePrice(value, n) {
-   document.n.prix.value=prxProd[value];
+    if(value == -1)
+        document.forms[n.name].prix.value = 0
+    else {
+        document.forms[n.name].prix.value = prxProd[value];
+        document.forms[n.name].quantite.value = 1;
+        //vérifie qu'il n'existe pas de ligne suivante, si elle existe ne fait rien
+        if(n.name.split('carte')[1]*1+1==nbProduit)
+            affLigne();
+    }
 }
 
+//document.'+n+'.prix.value=prxProd[this.value]
