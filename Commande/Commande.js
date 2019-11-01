ddb4366649b658b9212074f9b12bb98ea2512366
BlackFox3000 <flo-ti@hotmail.fr>
2019-11-01 12:28:07 +0100

    Commande
    Peaufinage code

--- Commande/Commande.js
+++ Commande/Commande.js
@@ -1,4 +1,4 @@
-var libProd = ['lait', 'cereales', 'pancake'], prxProd = [1.70, 2.90, 4.90], nbProduit = 0, TVA=19.5;
+var libProd = ['lait', 'cereales', 'pancake'], prxProd = [1.70, 2.90, 4.90], nbProduit = 0, TVA = 19.5;
 
 /**
  * afficher le formulaire.
@@ -28,10 +28,10 @@ function afficheForm() {
     document.getElementsByTagName("body")[0].innerHTML = authentification + '<div class="liste"></div>';
     affLigne();
 
-    document.getElementsByTagName("body")[0].innerHTML +='<div class="total"> <form name="total">' +
+    document.getElementsByTagName("body")[0].innerHTML += '<div class="total"> <form name="total">' +
         '<label for="montantHT">Montant Hors Taxe: </label>' +
         '<input type="text" name="montantHT" readonly>' +
-        '<label for="montantTVA">TVA '+TVA+'%: </label>' +
+        '<label for="montantTVA">TVA ' + TVA + '%: </label>' +
         '<input type="text" name="montantTVA" readonly>' +
         '<label for="montantTTC">Montant TTC: </label>' +
         '<input type="text" name="montantTTC" readonly>' +
@@ -40,127 +40,22 @@ function afficheForm() {
     document.getElementsByTagName("body")[0].innerHTML +=
         '<div id="option"> <input type="button" value="Recommencer" onclick="resetForm()">' +
         '<input type="button" value="Valider" onclick="validationCmd()">' +
-        ' </div>';
+        '</div>';
 
     affMontant();
 }
 
 /**
- * La fonction plus(n) permettra d'augmenter la quantité de la ligne n (d'une unité) et
- * réactualisera les montants. Elle sera appelée quand on cliquera sur les boutons +
- * @param n
- * @returns {boolean}
+ * Controle que le numéro ne dépasse pas 10 chiffres et place les points pour le numéro de téléphone
  */
-function plus(n) {
-    n = 'carte' + n;
-    let value = document.forms[n].produits.value;
-    if (Number(document.forms[n].prix.value).toFixed(2) != 0 && Number(document.forms[n].prix.value != undefined).toFixed(2)) {
-        document.forms[n].prix.value = Number(prxProd[value] * 1 + document.forms[n].prix.value * 1).toFixed(2);
-    }
-    document.forms[n].quantite.value++;
-    affMontant();
-}
-
-/**
- * La fonction moins(n) permettra de diminuer la quantité de la ligne n (d'une unité) et
- * réactualisera les montants. Elle sera appelée quand on cliquera sur les boutons –
- * @param n
- * @returns {boolean}
- */
-function moins(n) {
-    n = 'carte' + n;
-    //console.log(document.forms[n]);
-    let value = document.forms[n].produits.value;
-    if (document.forms[n].prix.value * 1 > value * 1 && document.forms[n].prix.value != 0) {
-        document.forms[n].prix.value = Number(document.forms[n].prix.value - prxProd[value]).toFixed(2);
-        document.forms[n].quantite.value--;
-        if (document.forms[n].prix.value == 0 || document.forms[n].prix.value < 0) {
-            console.log(n.split('carte')[1])
-            sup(parseInt(n.split('carte')[1]))
-        }
-
-
-    }
-    affMontant();
-}
-
-
-/**
- * La fonction sup(n) effacera les données de la ligne n. Elle sera appelée quand on cliquera sur
- * les boutons x (on ne prévoit pas de remonter les lignes)
- * @param n
- * @returns {boolean}
- */
-function sup(n) {
-    n = 'carte' + n
-    console.log(n)
-    document.forms[n].produits.value = -1;
-    document.forms[n].prix.value = 0;
-    document.forms[n].quantite.value =0;
-
-    affMontant();
-}
-
-/**
- * La fonction affMontant() permettra de calculer les 3 montants de la commande et de les
- * afficher.
- * @returns {boolean}
- */
-function affMontant() {
-    let montantHT=0;
-    for(let i=0; i<nbProduit; i++){
-        let form='carte'+i;
-        let value=document.forms[form].prix.value
-        if(value == undefined)
-            value=0;
-        montantHT=Number(value*1+montantHT*1).toFixed(2);
-    }
-    document.total.montantHT.value= Number(montantHT).toFixed(2);
-    document.total.montantTVA.value= Number(montantHT/100*TVA).toFixed(2);
-    document.total.montantTTC.value=Number(document.total.montantTVA.value*1+document.total.montantHT.value*1).toFixed(2);
-}
-
-/**
- * La fonction validationCmd() permettra de vérifier si le formulaire a été correctement rempli
- * et affichera éventuellement toutes les erreurs.( Rappel: \n permet un saut à la ligne dans une
- * alerte )
- */
-function validationCmd() {
-    //console.log(document.identification)
-    let codePost=document.identification.codePostal.value;
-    let numTel=document.identification.tel.value;
-    let mail=document.identification.mail.value
-    let erreur='';
-    if(! codePostalIsValide(codePost)) erreur+="Code postal invalide! \n";
-    if(! numTelIsValide(numTel)) erreur +="Num tel invalide! \n";
-    if(! mailIsValide(mail)) erreur +="Mail invalide! \n";
-    if(! formIsNotEmpty(document.identification)) erreur +="formulaire incomplet!! \n";
-    console.log('functon:'+ formIsNotEmpty(document.identification))
-    if(erreur == '') alert("cm valide");
-    else
-        alert(erreur);
-}
-
-/**
- * La fonction affLigne(n) permettra d'afficher le prix du produit sélectionné à la ligne n et
- * initialisera la quantité à 1. Les montants seront actualisés. Elle sera appelée chaque fois que
- * les valeurs des listes déroulantes changeront
- * @param n
- */
-function affLigne() {
-    let index = nbProduit, nom='carte';
-    let form = '<br>' + nbProduit + '<form name="carte' + nbProduit + '" id="carte' + index + '"  >\n' +
-        generatorListeDeroulante("carte" + index) +
-        '<input name="prix" value=0 readonly>' +
-        '<input name="quantite" value=0 readonly>'+
-        ' <input type="button" value="+" onclick="plus(' + index + ')">' +
-        ' <input type="button" value="-" onclick="moins(' + index + ')"> ' +
-        ' <input type="button" value="x" onclick="sup(' +index + ')"> ' +
-        '</form>';
-    nbProduit++;
-    let wrapper = document.createElement("div");
-    wrapper.innerHTML = form;
-    document.getElementsByClassName("liste")[0].appendChild(wrapper);
+function ctrlInput() {
+    console.log('test')
+    let length = document.identification.tel.value.length;
+    if (length === 2 || length === 5 || length === 8 || length === 11)
+        document.identification.tel.value += '.'
+    //empêche d'entrer un numéro de téléphone > 10
+    if (length > 14)
+        document.identification.tel.value = document.identification.tel.value.substr(0, 14);
 }
 
 /**
@@ -169,7 +64,7 @@ function affLigne() {
  * @returns {boolean}
  */
 function codePostalIsValide(code) {
-    let reg = new RegExp(/^[0-9]{5}/)
+    let reg = new RegExp(/^[0-9]{5}/);
     return reg.test(code);
 }
 
@@ -193,6 +88,95 @@ function mailIsValide(mail) {
     return reg.test(mail);
 }
 
+
+/**
+ * La fonction plus(n) permettra d'augmenter la quantité de la ligne n (d'une unité) et
+ * réactualisera les montants. Elle sera appelée quand on cliquera sur les boutons +
+ * @param n
+ * @returns {boolean}
+ */
+function plus(n) {
+    n = 'carte' + n;
+    let value = document.forms[n].produits.value;
+    if (Number(document.forms[n].prix.value).toFixed(2) !== 0 && Number(document.forms[n].prix.value !== undefined).toFixed(2)) {
+        document.forms[n].prix.value = Number(prxProd[value] * 1 + document.forms[n].prix.value * 1).toFixed(2);
+    }
+    document.forms[n].quantite.value++;
+    affMontant();
+}
+
+/**
+ * La fonction moins(n) permettra de diminuer la quantité de la ligne n (d'une unité) et
+ * réactualisera les montants. Elle sera appelée quand on cliquera sur les boutons –
+ * @param n
+ * @returns {boolean}
+ */
+function moins(n) {
+    n = 'carte' + n;
+    //console.log(document.forms[n]);
+    let value = document.forms[n].produits.value;
+    if (document.forms[n].prix.value * 1 > value * 1 && document.forms[n].prix.value !== 0) {
+        document.forms[n].prix.value = Number(document.forms[n].prix.value - prxProd[value]).toFixed(2);
+        document.forms[n].quantite.value--;
+        if (document.forms[n].prix.value === 0 || document.forms[n].prix.value < 0)
+            sup(parseInt(n.split('carte')[1]));
+    }
+    affMontant();
+}
+
+/**
+ * La fonction sup(n) effacera les données de la ligne n. Elle sera appelée quand on cliquera sur
+ * les boutons x (on ne prévoit pas de remonter les lignes)
+ * @param n
+ * @returns {boolean}
+ */
+function sup(n) {
+    n = 'carte' + n;
+    document.forms[n].produits.value = -1;
+    document.forms[n].prix.value = 0;
+    document.forms[n].quantite.value = 0;
+    affMontant();
+}
+
+/**
+ * La fonction affMontant() permettra de calculer les 3 montants de la commande et de les
+ * afficher.
+ * @returns {boolean}
+ */
+function affMontant() {
+    let montantHT = 0;
+    for (let i = 0; i < nbProduit; i++) {
+        let form = 'carte' + i, value = document.forms[form].prix.value;
+        if (value === undefined)
+            value = 0;
+        montantHT = Number(value * 1 + montantHT * 1).toFixed(2);
+    }
+    document.total.montantHT.value = Number(montantHT).toFixed(2);
+    document.total.montantTVA.value = Number(montantHT / 100 * TVA).toFixed(2);
+    document.total.montantTTC.value = Number(document.total.montantTVA.value * 1 + document.total.montantHT.value * 1).toFixed(2);
+}
+
+/**
+ * La fonction affLigne(n) permettra d'afficher le prix du produit sélectionné à la ligne n et
+ * initialisera la quantité à 1. Les montants seront actualisés. Elle sera appelée chaque fois que
+ * les valeurs des listes déroulantes changeront
+ */
+function affLigne() {
+    let index = nbProduit;
+    let form = '<br>' + nbProduit + '<form name="carte' + nbProduit + '" id="carte' + index + '"  >\n' +
+        generatorListeDeroulante("carte" + index) +
+        '<input name="prix" value=0 readonly>' +
+        '<input name="quantite" value=0 readonly>' +
+        ' <input type="button" value="+" onclick="plus(' + index + ')">' +
+        ' <input type="button" value="-" onclick="moins(' + index + ')"> ' +
+        ' <input type="button" value="x" onclick="sup(' + index + ')"> ' +
+        '</form>';
+    nbProduit++;
+    let wrapper = document.createElement("div");
+    wrapper.innerHTML = form;
+    document.getElementsByClassName("liste")[0].appendChild(wrapper);
+}
+
 /**
  * Génère la liste déroulante pour un bouton
  * @returns {string}
@@ -211,48 +195,59 @@ function generatorListeDeroulante(n) {
 /**
  * Place le prix correspondant au produit sélectionner dans la case prix
  * @param value
+ * @param n
  */
 function takePrice(value, n) {
-    if(value == -1)
-        document.forms[n.name].prix.value = 0
+    if (value == -1)
+        document.forms[n.name].prix.value = 0;
     else {
         document.forms[n.name].prix.value = prxProd[value];
         document.forms[n.name].quantite.value = 1;
         //vérifie qu'il n'existe pas de ligne suivante, si elle existe ne fait rien
-        if(n.name.split('carte')[1]*1+1==nbProduit)
+        if (n.name.split('carte')[1] * 1 + 1 == nbProduit)
             affLigne();
     }
 }
 
-/**
- * Controle que le numéro ne dépasse pas 10 chiffres et place les points pour le numéro de téléphone
- */
-function ctrlInput() {
-    console.log('test')
-    let length =document.identification.tel.value.length;
-    if(length==2 || length==5 || length==8 || length==11)
-            document.identification.tel.value+='.'
-    //empêche d'entrer un numéro de téléphone > 10
-    if(length>14)
-        document.identification.tel.value= document.identification.tel.value.substr(0,14);
-}
 
 /**
  * Vérifie que le formulaire à toutes ses cases remplies
- * @param formulaire
+ * @param n
  * @returns {boolean}
  */
 function formIsNotEmpty(n) {
-    for(let i=0; i<n.length; i++ )
-        if(n[i].value == '')
-            return false
-    return  true;
+    for (let i = 0; i < n.length; i++)
+        if (n[i].value === '')
+            return false;
+    return true;
 }
 
+/**
+ * Rénitialise le formulaire des commande uniquement!
+ */
 function resetForm() {
-   // console.log( document.getElementsByClassName('liste')[0].innerHTML)
-    document.getElementsByClassName('liste')[0].innerHTML='';
-    nbProduit=0;
+    document.getElementsByClassName('liste')[0].innerHTML = '';
+    nbProduit = 0;
     affMontant();
     affLigne();
+}
+
+/**
+ * La fonction validationCmd() permettra de vérifier si le formulaire a été correctement rempli
+ * et affichera éventuellement toutes les erreurs.( Rappel: \n permet un saut à la ligne dans une
+ * alerte )
+ */
+function validationCmd() {
+    //console.log(document.identification)
+    let codePost = document.identification.codePostal.value;
+    let numTel = document.identification.tel.value;
+    let mail = document.identification.mail.value;
+    let erreur = '';
+    if (!codePostalIsValide(codePost)) erreur += "Code postal invalide! \n";
+    if (!numTelIsValide(numTel)) erreur += "Num tel invalide! \n";
+    if (!mailIsValide(mail)) erreur += "Mail invalide! \n";
+    if (!formIsNotEmpty(document.identification)) erreur += "formulaire incomplet!! \n";
+    if (erreur === '') alert("cm valide");
+    else
+        alert(erreur);
 }
