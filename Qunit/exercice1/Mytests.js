3cddae2e5733d27e1417d5c949f006ff9289cc8e
BlackFox3000 <flo-ti@hotmail.fr>
2020-01-15 14:24:04 +0100

    Les promesses Nouveau chapitre

--- Qunit/exercice1/Mytests.js
+++ Qunit/exercice1/Mytests.js
@@ -3,31 +3,34 @@
  * */
 
 //exemple fonction booleen
-QUnit.test("fonction testée: mD3()", function(assert) {
-    assert.ok(mD3(0), '0 est un nombre multiple de 3');
-    assert.ok(mD3 (3), '3 est un nombre multiple de 3');
-    assert.ok(mD3 (6), '6 est un nombre multiple de 3');
-    assert.ok(!mD3 (5), '5 n\'est pas multiple de 3');
-    assert.ok(!mD3("essai"),' essai n\'est pas un multiple de 3');
+// QUnit.test("fonction testée: mD3()", function(assert) {
+//     assert.ok(mD3(0), '0 est un nombre multiple de 3');
+//     assert.ok(mD3 (3), '3 est un nombre multiple de 3');
+//     assert.ok(mD3 (6), '6 est un nombre multiple de 3');
+//     assert.ok(!mD3 (5), '5 n\'est pas multiple de 3');
+//     assert.ok(!mD3("essai"),' essai n\'est pas un multiple de 3');
+//
+// });
+carre = new Objet("Carré", 5);
+carre_neg = new Objet("Carré", -5);
+carre_text = new Objet("Carré", "ihoihoi")
 
-});
+cercle = new Objet("Cercle",5)
+rect = new Objet("Rectangle", 5, 10)
 
-
-//exemple d'autres fonctions
+//Test résultat numérique
 QUnit.test("fonction testée: carreNombre()", function(assert){
-    assert.equal(carreNombre(5),25, '25 carré de 5');
-    assert.equal(carreNombre(-5),25, '25 carré de -5');
-    assert.equal(carreNombre("ddd"),-1, 'Param: Pas un nombre');
+    assert.equal(carre.surface(),25, '25 surface de carré de 5');
+    assert.equal(carre_neg.surface(),25, '25 surface de carré de -5');
+    assert.equal(carre_text.surface(),-1, 'Param: Pas un nombre');
+    assert.equal(surface(carre),25, '25 surface de carré de 5');
+    assert.equal(surface(rect),50, '50 surface de carré de 5');
+    assert.equal(surface(cercle),5*5*Math.PI, '25 surface de carré de 5');
+    assert.equal(surface({"forme":"Rectangle","longueur":100,"largeur":50}),500, '25 surface de carré de 5');
+
 })
 
-//structures de données
-QUnit.test("fonction testée: puissanceNombre()", function(assert){
-    assert.deepEqual(puissanceNombre(5),
-        {"n":5,"P2":25,"P3":125,"P4":625}, 'On obtient les puissances de 5');
-    assert.deepEqual(puissanceNombre(-5), {"n":-5,"P2":25,"P3":-
-            125,"P4":625}, 'On obtient les puissances de 5');
-    assert.deepEqual(puissanceNombre("ddd"),{"n":"ddd","P2":-1,"P3":-
-            1,"P4": -1}, 'Param: Pas un nombre');
-})
+
+
 
 
--- Qunit/exercice1/myProject.js
+++ Qunit/exercice1/myProject.js
@@ -20,30 +20,61 @@ class Objet{
     largeur;
     rayon;
 
+    /**
+     * @param "Carré", "Rectangle", "Cercle"
+     * @param coté/rayon/longueur
+     * @param largeur
+     */
+    constructor(forme, param1 , param2){
+        //if param1 is not number -> object is not form
+        if(typeof(param1)==="number"){
+            //if param2 is define and is not number -> object is not form
+            if(param2 !== undefined &&  typeof(param2)!=="number")
+                return null;
+            else{
+                if( forme === "Carré" && undefined !== param1)
+                {
+                    this.forme = forme;
+                    this.cote = param1;
+                }
+                if(forme === "Rectangle" && undefined !== param1 && undefined !== param2)
+                {
+                    this.forme = forme;
+                    this.longueur = param1;
+                    this.largeur = param2;
+                }
+                if(forme === "Cercle" && undefined !== param1)
+                {
+                    this.forme = forme;
+                    this.rayon = param1;
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Retourne la surface d'un objet
+     * @returns {number}
+     */
     surface(){
-        if(this.forme == "Carré" && !undefined(this.cote))
+        if(this.forme === "Carré" && undefined !== this.cote)
             return this.cote*this.cote;
-        if(this.forme == "Rectangle" && !undefined(this.largeur) && !undefined(this.longueur))
-            return ;
-        if(this.forme == "Cercle" && !undefined(this.rayon))
-            return ;
+        if(this.forme === "Rectangle" && undefined !== this.largeur && undefined !== this.longueur)
+            return this.longueur * this.largeur;
+        if(this.forme === "Cercle" && undefined !== this.rayon)
+            return this.rayon*this.rayon*Math.PI ;
         return -1;
     }
 }
 
 /**
- * Écrire la fonction surface(param) qui retournera la surface des formes dont les dimensions seront
- passées dans le paramètre.
- Le paramètre doit être un objet ayant les propriétés suivantes:
- "forme" : pourra prendre les valeurs suivantes ("Carré", "Rectangle", "Cercle")
-           Si la propriété "forme" = "Carré" alors une propriété "coté" indiquera la longueur du côté du carré.
-           Si la propriété "forme" = "Rectangle" alors une propriété "longueur" indiquera la longueur du
-           rectangle et une propriété "largeur" indiquera la largeur du rectangle.
- Enfin si la propriété "forme" = "Cercle" alors une propriété "rayon" indiquera le rayon du cercle
- La fonction surface(param) retournera la surface de la figure si le paramètre est un objet bien
- structuré et -1 dans le cas contraire.
- * @objet param { x, y, z }
+ * @objet param
  */
  function surface(param) {
-
+     console.log(param)
+     console.log(param instanceof Objet)
+    return param.surface();
 }
+
+
